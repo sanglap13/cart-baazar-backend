15fd107da8a31132a4d6478a8ca263b8a645d018
sanglap13 <sanglapmridha@gmail.com>
2024-07-03 22:22:13 +0530

    chore: add dotenv and morgan

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,21 @@ import express from "express";
 import { connectDB } from "./utils/db/db.js";
 import { errorMiddleware } from "./middlewares/errorHandler.js";
 import NodeCache from "node-cache";
+import { config } from "dotenv";
+import morgan from "morgan";
 
 // Importing Routes";
 import Routes from "./routes/index.routes.js";
 
-const port = 4000;
+config({
+  path: "./.env",
+});
+
+const port = process.env.PORT || 4000;
 
 //db connection
-connectDB("mongodb://localhost:27017");
+const mongoURI = process.env.MONGO_URI!;
+connectDB(mongoURI);
 
 export const myCache = new NodeCache();
 
@@ -17,6 +24,7 @@ const app = express();
 
 //middlewares
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
