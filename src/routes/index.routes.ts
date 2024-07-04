6a4c7a6adef869f412c505ed482cb85cf328568d
sanglap13 <sanglapmridha@gmail.com>
2024-07-04 12:21:31 +0530

    feat: add payment apis

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -3,11 +3,13 @@ import express from "express";
 import UserRoutes from "./user/user.routes.js";
 import ProductRoutes from "./product/product.routes.js";
 import OrderRoutes from "./order/order.routes.js";
+import PaymentRoutes from "./payment/payment.routes.js";
 
 const app = express();
 
 app.use("/user", UserRoutes);
 app.use("/product", ProductRoutes);
 app.use("/order", OrderRoutes);
+app.use("/payment", PaymentRoutes);
 
 export default app;
---
+++ src/routes/payment/payment.routes.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { adminOnlyAccess } from "../../middlewares/auth.js";
+import {
+  allCoupons,
+  applyDiscount,
+  deleteCoupon,
+  getCoupon,
+  newCoupon,
+  updateCoupon,
+} from "../../controllers/payment/payment.controller.js";
+
+const router = express.Router();
+
+// route - /api/v1/payment/create
+// router.post("/create", createPaymentIntent);
+
+// route - /api/v1/payment/coupon/new
+router.get("/discount", applyDiscount);
+
+// route - /api/v1/payment/coupon/new
+router.post("/coupon/new", adminOnlyAccess, newCoupon);
+
+// route - /api/v1/payment/coupon/all
+router.get("/coupon/all", adminOnlyAccess, allCoupons);
+
+// route - /api/v1/payment/coupon/:id
+router
+  .route("/coupon/:id")
+  .get(adminOnlyAccess, getCoupon)
+  .put(adminOnlyAccess, updateCoupon)
+  .delete(adminOnlyAccess, deleteCoupon);
+
+export default router;
